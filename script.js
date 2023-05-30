6a1eb2be2bb0cc5eb7446e67a34cbd4fe69744ae
MEYVIN <www.meyer12@gmail.com>
2023-05-29 21:26:46 -0500

    Agregando desencriptacion

--- script.js
+++ script.js
@@ -35,6 +35,35 @@ function copiar() {
         console.log('ERROR al intentar copiar el email');
     }
 }
+function desencriptar() {
+    let temporal = "";
+    let texto = document.querySelector("#entrada").value;
+    let i = 0;
+    while (i < texto.length) {
+        if (texto[i] + texto[i + 1] == "ai") {
+            temporal += "a";
+            i += 2;
+        } else if (texto[i] + texto[i + 1] + texto[i + 2] + texto[i + 3] + texto[+4] == "enter") {
+            temporal += "e";
+            i += 5;
+        } else if (texto[i] + texto[i + 1] + texto[i + 2] + texto[i + 3] == "imes") {
+            temporal += "i";
+            i += 4;
+        } else if (texto[i] + texto[i + 1] + texto[i + 2] + texto[i + 3] == "ober") {
+            temporal += "o";
+            i += 4;
+        } else if (texto[i] + texto[i + 1] + texto[i + 2] + texto[i + 3] == "ufat") {
+            temporal += "u";
+            i += 4;
+        } else {
+            temporal = temporal + texto[i];
+            i++;
+        }
+    }
+    salida.textContent = temporal;
+    salida.style.background = "#fff";
+}
 
 btnEncript.addEventListener("click", encriptador);
+btnDesencr.addEventListener("click", desencriptar);
 btnCopiar.addEventListener("click", copiar);
